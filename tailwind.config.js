5169d12dbed0c2ef5b958bdc13d1220c70c20c46
taiwoj121 <taiwoj121@gmail.com>
2022-10-13 13:08:12 +0100

    Changes in the config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{js,jsx}"],
   theme: {
+    fontFamily: {
+      roboto: ['Roboto', defaultTheme.fontFamily.sans]
+    },
+    colors:{
+      primary:{
+        purple_dark:'#240D57',
+        purple_light:'#240D57',
+        evited_purple:'#8456EC',
+        Evited_pink:'#E87BF8',
+      },
+    },
     extend: {},
   },
   plugins: [],
