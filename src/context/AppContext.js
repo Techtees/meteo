d388d9cda5c5e7bb0ce1c1b5499edfe760d4169e
taiwoj121 <taiwoj121@gmail.com>
2022-10-14 13:29:57 +0100

    refacor code

---
+++ src/context/AppContext.js
@@ -0,0 +1,58 @@
+import { createContext, useState } from "react";
+import {city} from '../data/city'
+
+const AppContext = createContext()
+
+export const AppProvider = ({children}) => {
+
+    const [inputValue, setInputValue] = useState()
+    const [cityData, setCitydata] = useState(city)
+    const [favourite, setFavourite] = useState ([])
+    const [starredIcon, setStarredIcon] = useState()
+
+    // add favourite
+    const addFavourite = (item, lat, lng) => {
+       
+        setFavourite(
+             (favourite) => {
+                 // used lat and lng as a check because it is unique
+                 const exists = favourite.find((item) => item.lat === lat && item.lng === lng)
+                 if(!exists) {
+                     return [...favourite, {item, lat, lng }]
+                 } else
+              return favourite
+             } 
+            
+        )
+     }
+
+     //remove favourite
+     const removeFavourite = (lat, lng) => {
+        
+        setFavourite(
+            (favourite) => {
+                // used lat and lng as a check because it is unique
+                return favourite.filter((item) => item.lat !== lat && item.lng !== lng)
+            } 
+           
+       )
+     }
+     
+     const handleChange = (e) => {
+        const inputValue = e.target.value.toLowerCase();
+        setInputValue(inputValue)
+    }
+    
+    return <AppContext.Provider value={{
+        cityData,
+        inputValue,
+        handleChange,
+        addFavourite,
+        removeFavourite,
+        favourite,
+    }} >
+      {children}
+    </AppContext.Provider>
+}
+
+export default AppContext;
