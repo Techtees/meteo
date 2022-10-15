f25756a3ca3cdb6be6009b422e74757803741e78
taiwoj121 <taiwoj121@gmail.com>
2022-10-15 16:23:45 +0100

    updated read me

--- src/data/api/weather.js
+++ src/data/api/weather.js
@@ -1,14 +1,4 @@
 
-
-//  async function getLatLong() {
-//     const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m');
-//     const responseData = await response.json();
-//     console.log(responseData)
-
-// }
-
-// getLatLong() 
-
 const buildQuery = (settings) => {
     const url = new URL(`https://api.open-meteo.com/v1/forecast`);
 
@@ -20,13 +10,14 @@ const buildQuery = (settings) => {
 }
 
 
-
-async function getAllWeatherData(settings, lat, long) {
+export async function getAllWeatherData(settings = {}, lat, long) {
     const url = buildQuery(settings)
     url.searchParams.append("latitude", lat)
     url.searchParams.append("longitude", long)
 
     // this.spinner.style.display = 'block';
+    
+    console.log(url.toString())
     const response = await fetch(url.toString())
 
     const responseData = await response.json();
