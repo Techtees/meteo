fe0c2de4735ce08b982877f3b046dcc50ed88fd8
taiwoj121 <taiwoj121@gmail.com>
2022-10-15 19:35:34 +0100

    removed un used prop btnstyle

--- src/components/utils/Button.jsx
+++ src/components/utils/Button.jsx
@@ -1,4 +1,4 @@
-function Button ({text, btnType, handleClick}) {
+function Button ({text, handleClick}) {
      return(
         <button type='button' className='max-w-[400px] border border-primary-purple hover:text-white hover:bg-primary-purple duration-500 rounded-md p-3 pr-[3rem] pl-[3rem] ' onClick={handleClick}>{text}</button>
      )
