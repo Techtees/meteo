bb26ae550e34417afe7d8a9a60192688f6b301b7
taiwoj121 <taiwoj121@gmail.com>
2022-10-15 19:11:04 +0100

    refactor to pass array values

--- src/data/api/weather.js
+++ src/data/api/weather.js
@@ -3,30 +3,19 @@ const buildQuery = (settings) => {
     const url = new URL(`https://api.open-meteo.com/v1/forecast`);
 
     Object.entries(settings).forEach(([key, value])=> {
-        url.searchParams.append(key, value);
+        url.searchParams.append(key, value.toString());
     })
 
     return url;
 }
 
 
-export async function getAllWeatherData(settings = {}, lat, long) {
-    const url = buildQuery(settings)
-    url.searchParams.append("latitude", lat)
-    url.searchParams.append("longitude", long)
+export async function getAllWeatherData(settings = {}, { view, data}) {
+    const url = buildQuery(settings).toString()
 
-    // this.spinner.style.display = 'block';
-    
-    console.log(url.toString())
-    const response = await fetch(url.toString())
+    const response = await fetch(`${url}&${view}=${data}`)
 
-    const responseData = await response.json();
-
-    return {
-      responseData,
-      
-    };
-
+    return await response.json();
   }
 
  
