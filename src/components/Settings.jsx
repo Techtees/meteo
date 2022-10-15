c6525aa809029a3538a906c1dd4fced565b2bafc
taiwoj121 <taiwoj121@gmail.com>
2022-10-15 07:46:41 +0100

    mapped input radio and select

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -1,21 +1,50 @@
 import MobileIcon from './utils/MobileIcon';
 import Button from './utils/Button'
+import SettingContext from '../context/SettingContext';
+import { useContext } from 'react';
 
 function Settings() {
 
+    const { 
+        handleTemperature,
+        handlePrecipitation,
+        handlePastDays,
+        handleTimezone,
+        handleWind,
+        tempUnit,windUnit,precipitationUnit,timezone,pastDays,
+        handleRevertSettings,
+    } = useContext(SettingContext)
+
+    // console.log(handleChange)
+
     const tempData = [
-        {id:1,label:'Celsius °C'},
-        {id:2,label:'Fahrenheit °F'}
+        {id:1,label:'Celsius °C', value: 'celsius'},
+        {id:2,label:'Fahrenheit °F', value: 'fahrenheit'}
     ]
     const windData = [
-        {id:1,label:'km/h'},
-        {id:2,label:'m/s'},
-        {id:3,label:'mph'},
-        {id:4,label:'kn'}
+        {id:1,label:'km/h', value: 'km/h'},
+        {id:2,label:'m/s', value: 'm/s'},
+        {id:3,label:'mph', value: 'mph'},
+        {id:4,label:'kn', value: 'kn'}
     ]
     const precipitationData = [
-        {id:1,label:'Milimeter'},
-        {id:2,label:'Inch'},
+        {id:1,label:'Milimeter',  value: 'mm'},
+        {id:2,label:'Inch', value: 'inch'},
+    ]
+
+    const timezoneOptions = [
+        {id:1, label:'UTC', value:'UTC'},
+        {id:2, label:'CEST', value:'CEST'},
+        {id:3, label:'GMT', value:'GMT'}
+    ]
+
+    const daysOptions = [
+        {id:0, label:'0', value: '0'},
+        {id:1, label:'1', value: '1'},
+        {id:2, label:'2', value: '2'},
+        {id:3, label:'3', value: '3'},
+        {id:4, label:'4', value: '4'},
+        {id:5, label:'5', value: '5'}
     ]
 
     return (
@@ -29,7 +58,7 @@ function Settings() {
                         {
                             tempData.map((data) => (
                                 <div key={data.id}>
-                                    <input type="radio" name='temp'/><label className='text-lg'> {data.label} </label>
+                                    <input type="radio" name='temp' value={data.value} defaultChecked={data.value ===tempUnit} onChange={(e) => handleTemperature(e)} /><label className='text-lg' > {data.label} </label>
                                 </div>
                             ))
                         }
@@ -39,7 +68,7 @@ function Settings() {
                         {
                             windData.map((data) => (
                                 <div key={data.id}>
-                                    <input type="radio" name='speed'/><label className='text-lg'> {data.label} </label>
+                                    <input type="radio" name='speed' value={data.value} defaultChecked={data.value === windUnit} onChange={(e) => handleWind(e)} /><label className='text-lg'> {data.label} </label>
                                 </div>
                             ))
                         }
@@ -49,29 +78,36 @@ function Settings() {
                         {
                             precipitationData.map((data) => (
                                 <div key={data.id}>
-                                    <input type="radio" name='precipitation'/><label className='text-lg'> {data.label} </label>
+                                    <input type="radio" name='precipitation' value={data.value} defaultChecked={data.value === precipitationUnit} onChange={(e) => handlePrecipitation(e)} /><label className='text-lg'> {data.label} </label>
                                 </div>
                             ))
                         }
                     </div>
-                    <p className="text-center mt-[2rem] md:mt-[4rem] text-xl italic">TImezone</p>   
+                    <p className="text-center mt-[2rem] md:mt-[4rem] text-xl italic">Timezone</p>   
                     <div className="flex flex-col justify-center items-center ">
-                        <select name="" id="" className="p-3 mt-2 md:mt-4  w-[70%] rounded-md border "> 
-                            <option value="">Hourly View</option>
-                            <option value="">Daily View</option>
+                        <select name="" id="" className="p-3 mt-2 md:mt-4  w-[70%] rounded-md border " onChange={(e) => handleTimezone(e)}> 
+                            {
+                                timezoneOptions.map((data) => (
+                                    <option key={data.id} selected={data.value === timezone} value={data.value}  >{data.label}</option>
+                                ))
+                            }
                         </select>
                     </div> 
                     <p className="text-center text-xl mt-6 italic">PastDays</p>   
                     <div className="flex flex-col justify-center items-center">
-                        <select name="" id="" className="p-3 mt-2 md:mt-4 rounded-md w-[70%]  border "> 
-                            <option value="">Hourly View</option>
-                            <option value="">Daily View</option>
+                        <select name="" id="" className="p-3 mt-2 md:mt-4 rounded-md w-[70%]  border " onChange={(e) => handlePastDays(e)}> 
+                            {
+                                 daysOptions.map((data) => (
+                                    <option key={data.id}  selected={data.value === pastDays} value={data.value}   >{data.label}</option>
+                                ))
+                            }
+                            {/* <option value="">Daily View</option> */}
                         </select>
                     </div>    
 
-                    <div className="btn mt-[4rem] md:mt-[6rem] flex  flex-col justify-center md:flex-row  gap-[2rem] md:gap-[5rem] items-center">
-                        <Button text='Revert settings to default'/>
-                        <Button text='Delete favourites'  />
+                    <div className="btn mt-[4rem] md:mt-[6rem] flex  flex-col justify-center md:flex-row  gap-[2rem] md:gap-[5rem] items-center" >
+                        <Button text='Revert settings to default' handleClick={handleRevertSettings} />
+                        <Button text='Delete favourites' />
                     </div>                 
                 </form>
             </div>
